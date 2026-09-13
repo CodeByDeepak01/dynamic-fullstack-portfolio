7f7e6a5957c132143e1f2eb8469cc04184ffc47a
Deepak <dpal789557@gmail.com>
2026-09-13 17:56:24 +0530

    Fix production CORS

--- portfolio-backend/src/main/java/com/portfolio/portfolio_backend/config/SecurityConfig.java
+++ portfolio-backend/src/main/java/com/portfolio/portfolio_backend/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
 
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(
--- portfolio-backend/src/main/java/com/portfolio/portfolio_backend/config/WebConfig.java
+++ portfolio-backend/src/main/java/com/portfolio/portfolio_backend/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.portfolio.portfolio_backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,11 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addResourceHandlers(
-            ResourceHandlerRegistry registry
-    ) {
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://deepak-portfolio-jcg2.onrender.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
